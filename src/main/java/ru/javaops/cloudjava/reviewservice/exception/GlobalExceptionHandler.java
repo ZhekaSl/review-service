5bc812c7402c9cc634b8e27e98b03fcf0af3b2af
Zheka Slepko <zhenya.slepko@gmail.com>
2026-02-14 19:45:33 +0200

    refactor: delete spring cloud and eureka.

--- src/main/java/ru/javaops/cloudjava/reviewservice/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/javaops/cloudjava/reviewservice/exception/GlobalExceptionHandler.java
@@ -53,7 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleHandlerMethodValidationException(HandlerMethodValidationException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         var pd = ex.getBody();
         Map<String, String> errors = new HashMap<>();
-        ex.getAllValidationResults().forEach(result -> {
+        ex.getParameterValidationResults().forEach(result -> {
             result.getResolvableErrors().forEach(e -> {
                 errors.put(result.getMethodParameter().getParameterName(), e.getDefaultMessage());
             });
